7c7ac35c23ae10e8e3a7ac67c88dff117e85ca63
Ellen201906 <51275255+Ellen201906@users.noreply.github.com>
2019-06-06 20:08:12 -0600

    c

--- packages.php
+++ packages.php
@@ -24,21 +24,10 @@ echo <<<GO
       	       
        <div class="card" style="height:430px">
              <img src=$row[7] alt="Vacation Packages" style="width:100%">
-                <div class="text"> 
-                  <h5>$row[1]</h5>
-                    <p class="date">$row[2] <br>  
-                      $row[3]</p>
-                    <p class="price">ONLY $$row[5]</p>
-                    <p>$row[4]<p
-	    
-                <form method='post' action='package$row[0].php'>
-                  <input class="book btn-outline-success" type='submit' value='Details' name=$row[0]></input>
-                </form>
-		
-           
-        
-GO;
-	   
+	     <div class="counter">
+            <i class="fa fa-eye"></i>
+            <!-- this part is recording the amount the times package one uploads and writes the a record in to a file saved in the database -->
+            <?php
             // Create a variable //
                $hits = file("counter1.txt");
                //Add one to the file //
@@ -50,17 +39,27 @@ GO;
                //Close file// 
                fclose($fp);
                echo  $hits[0].'clicks';
-	       print '</div>';
-	       print '</div>';
-                
-              
-			     
+                ?>
+            </div>
+	     
+                <div class="text"> 
+                  <h5>$row[1]</h5>
+                    <p class="date">$row[2] <br>  
+                      $row[3]</p>
+                    <p class="price">ONLY $$row[5]</p>
+                    <p>$row[4]<p
 	    
-      }
-     
-             
-?>
-    
-          
+                <form method='post' action='package$row[0].php'>
+                  <input class="book btn-outline-success" type='submit' value='Details' name=$row[0]></input>
+                </form>
+		</div>
+		</div>
+		
+           
+        
+GO;
+	   
+   }                  
+?>          
           
 
